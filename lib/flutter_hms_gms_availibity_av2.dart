d25ccf79414ffea0573facba47ea69c7788ca099
Mayank Janmejay <info@mayank-j.com>
2022-11-28 16:45:46 +0200

    add comments

--- lib/flutter_hms_gms_availibity_av2.dart
+++ lib/flutter_hms_gms_availibity_av2.dart
@@ -8,7 +8,8 @@ import 'package:flutter/services.dart';
 class FlutterHmsGmsAvailability {
   static const MethodChannel _channel =
       const MethodChannel('flutter_hms_gms_availability');
-
+      
+/// Check if GMS/ HMS is available for platform. Web check returns false & UnimplementedError()
   static Future<bool?> get isHmsAvailable async {
     if (kIsWeb)
     {
@@ -19,7 +20,14 @@ class FlutterHmsGmsAvailability {
     return await _channel.invokeMethod('isHmsAvailable');
   }
 
+/// Check if GMS/ HMS is available for platform. Web check returns false & UnimplementedError()
   static Future<bool> get isGmsAvailable async {
+    if (kIsWeb)
+    {
+      throw UnimplementedError(
+        'Unable to detect GMS/ HMS on Flutter web. Please integrate FCM in /web flutter project'
+      );     
+    }
     return await _channel.invokeMethod('isGmsAvailable');
   }
 }
