095d79cf3937d547f5a52a9432b3ae5a1d3ef316
Mayank Janmejay <info@mayank-j.com>
2022-11-28 16:03:16 +0200

    Update
    Update plugin to Android V2 Embedding
    Throw Unimplemented Expectation on web

--- lib/flutter_hms_gms_availibity_av2.dart
+++ lib/flutter_hms_gms_availibity_av2.dart
@@ -1,12 +1,21 @@
+library flutter_hms_gms_availibity_av2;
 import 'dart:async';
-
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+/// A Calculator.
+
 
 class FlutterHmsGmsAvailability {
   static const MethodChannel _channel =
       const MethodChannel('flutter_hms_gms_availability');
 
-  static Future<bool> get isHmsAvailable async {
+  static Future<bool?> get isHmsAvailable async {
+    if (kIsWeb)
+    {
+      throw UnimplementedError(
+        'Unable to detect GMS/ HMS on Flutter web. Please integrate FCM in /web flutter project'
+      );     
+    }
     return await _channel.invokeMethod('isHmsAvailable');
   }
 
